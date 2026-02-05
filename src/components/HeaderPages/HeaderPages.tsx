7bfb20505f788bf7caf672f8feda0ddb8d1c5c84
Isabel Sáenz <isasaete@gmail.com>
2026-02-05 17:49:06 +0100

    Refactor HeaderPages component and change button for NavLink

--- src/components/HeaderPages/HeaderPages.tsx
+++ src/components/HeaderPages/HeaderPages.tsx
@@ -1,26 +1,23 @@
-import { Button } from "../shared/Button/Button";
+import type { ReactNode } from "react";
 
 interface HeaderPagesProps {
   title: string;
   subtitle?: string;
-  buttonLabel?: string;
+  action?: ReactNode;
 }
 
 const HeaderPages: React.FC<HeaderPagesProps> = ({
   title,
-  buttonLabel,
   subtitle,
+  action,
 }) => {
   return (
     <div className="flex justify-between items-center mb-6">
       <div className="flex flex-col">
-        <h2 className="text-2xl font-bold">{title}s</h2>
+        <h2 className="text-2xl font-bold font-mono tracking-wide">{title}</h2>
         <span className="text-base font-light">{subtitle}</span>
       </div>
-      <Button className="gap-3 transition-all shadow-2xs shadow-primary/30 hover:bg-primary/50 hover:cursor-pointer">
-        <span className="text-xl">+</span>
-        <span>{buttonLabel}</span>
-      </Button>
+      {action}
     </div>
   );
 };
