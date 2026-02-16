ad5a77e6422c1b17e6fc3ab3679cf7bd771d46b2
Isabel Sáenz <isasaete@gmail.com>
2026-02-16 11:21:52 +0100

    Integrate useFilters hook and updated FiltersBar in StockPage

--- src/pages/StockPage/StockPage.tsx
+++ src/pages/StockPage/StockPage.tsx
@@ -5,75 +5,30 @@ import HeaderPages from "../../components/HeaderPages/HeaderPages";
 import { FilamentsTable } from "../../features/stock/components/FilamentsTable/FilamentsTable";
 import useStock from "../../features/stock/hooks/useStock";
 import FiltersBar from "../../features/stock/components/Filters/FiltersBar";
-import { filamentMaterials } from "../../features/stock/components/FilamentForm/constans/filamentOption";
-import type { FilamentMaterial } from "../../features/stock/types/types";
 import useSort, { type SortOption } from "../../features/stock/hooks/useSort";
+import useFilters from "../../features/stock/hooks/useFilters";
 
 const StockPage: React.FC = () => {
   const { filaments, loadStock } = useStock();
-  const [showFavorites, setShowFavorites] = useState(false);
-  const [showLowStock, setShowLowStock] = useState(false);
-  const [selectedMaterial, setSelectedMaterial] = useState<
-    FilamentMaterial | ""
-  >("");
-  const [searchTerm, setSearchTerm] = useState("");
+  const {
+    filters,
+    setFilter,
+    filteredFilamentData,
+    activeFilterLabels,
+    removeFilter,
+    clearFilters,
+  } = useFilters(filaments);
   const [sortBy, setSortBy] = useState<SortOption>("none");
 
   useEffect(() => {
     loadStock();
   }, [loadStock]);
 
-  const filteredFilaments = filaments.filter((filament) => {
-    const matchesFavorites = !showFavorites || filament.isFavorite;
-    const matchesLowStock =
-      !showLowStock ||
-      filament.currentWeightGrams < filament.lowStockThresholdGrams;
-    const matchesMaterial =
-      !selectedMaterial || filament.material === selectedMaterial;
-    const matchesSearch =
-      filament.brand.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      filament.material.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      filament.priceEurs?.toString().includes(searchTerm);
-
-    return (
-      matchesFavorites && matchesLowStock && matchesMaterial && matchesSearch
-    );
-  });
-
-  const sortedFilaments = useSort(filteredFilaments, sortBy);
-
-  const activeFilterLabels = [];
-
-  if (showFavorites) activeFilterLabels.push("Favoritos");
-  if (showLowStock) activeFilterLabels.push("Stock bajo");
-  if (selectedMaterial) {
-    const materialLabel = filamentMaterials.find(
-      (material) => material.value === selectedMaterial,
-    )?.label;
-
-    if (materialLabel) activeFilterLabels.push(materialLabel);
-  }
-
-  const handleRemoveFilter = (filterName: string) => {
-    if (filterName === "Favoritos") {
-      setShowFavorites(false);
-    } else if (filterName === "Stock bajo") {
-      setShowLowStock(false);
-    } else {
-      const material = filamentMaterials.find(
-        (material) => material.label === filterName,
-      );
-
-      if (material) {
-        setSelectedMaterial("");
-      }
-    }
-  };
+  const sortedFilaments = useSort(filteredFilamentData, sortBy);
 
   const handleClearFilters = () => {
-    setShowFavorites(false);
-    setShowLowStock(false);
-    setSelectedMaterial("");
+    clearFilters();
+    setSortBy("none");
   };
 
   return (
@@ -94,19 +49,13 @@ const StockPage: React.FC = () => {
         }
       />
       <FiltersBar
-        onToggleFavorites={() => setShowFavorites(!showFavorites)}
-        showFavorites={showFavorites}
-        onToggleLowStock={() => setShowLowStock(!showLowStock)}
-        showLowStock={showLowStock}
-        activeFilterLabels={activeFilterLabels}
-        onClearFilters={handleClearFilters}
-        onRemoveFilter={handleRemoveFilter}
-        setSelectedMaterial={setSelectedMaterial}
-        selectedMaterial={selectedMaterial}
-        setSearchTerm={setSearchTerm}
-        searchTerms={searchTerm}
+        filters={filters}
+        setFilter={setFilter}
         sortBy={sortBy}
         onSortChange={setSortBy}
+        activeFilterLabels={activeFilterLabels}
+        onClearFilters={handleClearFilters}
+        onRemoveFilter={removeFilter}
       />
       <FilamentsTable filaments={sortedFilaments} />
     </>
