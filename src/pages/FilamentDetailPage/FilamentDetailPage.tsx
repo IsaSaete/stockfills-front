1cae4ff32bfb2333d0caf927fbe2b55211f28d02
Isabel Sáenz <isasaete@gmail.com>
2026-02-19 17:34:09 +0100

    Integrate getFilamentById into FilamentDetailPage

--- src/pages/FilamentDetailPage/FilamentDetailPage.tsx
+++ src/pages/FilamentDetailPage/FilamentDetailPage.tsx
@@ -1,14 +1,23 @@
+import { useParams } from "react-router";
 import FilamentDetail from "../../features/stock/components/FilamentDetail/FilamentDetail";
-import type { FilamentDto } from "../../features/stock/types/types";
 
-interface FilamentDetailPageProps {
-  filament?: FilamentDto;
-}
+import { useEffect } from "react";
+import useStock from "../../features/stock/hooks/useStock/useStock";
 
-const FilamentDetailPage: React.FC<FilamentDetailPageProps> = ({
-  filament,
-}) => {
-  return <FilamentDetail filament={filament!} />;
+const FilamentDetailPage: React.FC = () => {
+  const { id: filamentId } = useParams();
+  const { isLoading, currentFilament, getFilamentById } = useStock();
+
+  useEffect(() => {
+    window.scroll({ top: 0, behavior: "instant" });
+
+    getFilamentById(filamentId!);
+  }, [filamentId, getFilamentById]);
+
+  if (isLoading) return <div>Cargando...</div>;
+  if (!currentFilament) return <div>No encontrado</div>;
+
+  return <FilamentDetail filament={currentFilament} />;
 };
 
 export default FilamentDetailPage;
