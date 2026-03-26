1a63b79def749fd1855f7e6a1ffb300202297c31
Isabel Sáenz <isasaete@gmail.com>
2026-03-26 11:31:24 +0100

    Add paginated printing history GET client

--- src/features/historyPrint/client/PrintingHistoryClient.ts/types.ts
+++ src/features/historyPrint/client/PrintingHistoryClient.ts/types.ts
@@ -1,8 +1,20 @@
-import type { CreateHistoryPrinting, PrintingHistoryDto } from "../../types";
+import type {
+  CreateHistoryPrinting,
+  PrintingHistoryDto,
+  PrintingHistoryResponses,
+} from "../../types";
+
+export interface GetPrintingHistoryParams {
+  page?: number;
+  limit?: number;
+}
 
 export interface PrintingHistoryClientStructure {
   recordConsumption: (
     filamentId: string,
     createHistory: CreateHistoryPrinting,
   ) => Promise<PrintingHistoryDto>;
+  getPrintingHistory: (
+    params?: GetPrintingHistoryParams,
+  ) => Promise<PrintingHistoryResponses>;
 }
